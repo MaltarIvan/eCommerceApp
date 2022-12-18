6f8fdfb59b821151f144b3497d2d962b493d87ad
Ivan Maltar <ivan.maltar@led-elektronika.hr>
2022-12-18 16:19:12 +0100

    update default routing

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -66,7 +66,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Movies}/{action=Index}/{id?}");
 
 // Seed Database
 AppDbInitializer.Seed(app);
